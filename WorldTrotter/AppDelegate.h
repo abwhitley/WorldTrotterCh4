8083e3b87de5b45776a8a3678e34da5e7fb29613
abwhitley <a.blakewhitley@gmail.com>
2016-09-11 21:21:01 -0400

    Initial Commit

---
+++ WorldTrotter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WorldTrotter
+//
+//  Created by Austins Work on 9/11/16.
+//  Copyright © 2016 AustinsIronYard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
